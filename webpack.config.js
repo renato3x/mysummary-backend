1ff521dfda4765e429a90c7a4b34509cf140da8d
renato3x <pereirarenato21@gmail.com>
2022-06-17 09:45:18 -0300

    webpack: include pdfs folder in build

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
 const webpack = require('webpack')
-const path = require('path')
 const nodeExternals = require('webpack-node-externals')
+const DotEnvPlugin = require('dotenv-webpack')
+
+const path = require('path')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   target: 'node',
   entry: path.join(__dirname, 'src', 'app.js'),
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist', 'js'),
     filename: 'app.bundle.js'
   },
+  plugins: [
+    new DotEnvPlugin({
+      path: path.join(__dirname, '.env')
+    })
+  ],
   module: {
     rules: [
       {
