d8b934fa15dd70f8e8eee69abe7515c475c5750b
renato3x <pereirarenato21@gmail.com>
2022-07-20 20:42:21 -0300

    tests: pdf generator service tests passed

--- src/services/pdfGeneratorService.js
+++
@@ -1,9 +0,0 @@
-import pdf from 'html-pdf'
-
-export default (html, src) => {
-  return new Promise((resolve, reject) => {
-    pdf.create(html).toFile(src, error => {
-      error ? reject(error) : resolve()
-    })
-  })
-}
